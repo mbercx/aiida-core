8ffd0e23d64e8b6bc21d9cb62e8dbd13c6dc8903
mbercx <marnik.bercx@uantwerpen.be>
2020-09-07 17:00:49 +0200

    Add extras to Group
    Add extras column to the database models for groups, with an empty
    dictionary as a default. Add the `ExtrasBackendEntity` mixin class
    to the `BackendGroup` to make sure the group backend classes for
    django and sqlalchemy have the extras methods. Add the
    corresponding extras methods to the frontend `Group` class.
    
    For SqlAlchemy: instead of using the constructor to set the default
    values of the attributes and extras for `DbNode` and `DbGroup`, use
    the Column declaration to do so. Generate the required migration
    files and make adjustments where necessary.
    
    Also fix some typos in the utils.py modules and add exception
    chaining for exceptions raised straight from the handling of a
    previous exception.

---
+++ aiida/backends/sqlalchemy/migrations/versions/0edcdd5a30f0_add_extras_to_group.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida-core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+# pylint: disable=no-member,invalid-name
+"""Add extras column to the db_dbgroup table.
+
+Revision ID: 0edcdd5a30f0
+Revises: bf591f31dd12
+Create Date: 2019-04-03 14:38:50.585639
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '0edcdd5a30f0'
+down_revision = 'bf591f31dd12'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """Upgrade: Add the extras column to the 'db_dbgroup' table"""
+    op.add_column(
+        'db_dbgroup', sa.Column('extras', postgresql.JSONB(astext_type=sa.Text()), server_default='{}', nullable=True)
+    )
+    op.alter_column('db_dbnode', 'extras', server_default='{}')
+    op.alter_column('db_dbnode', 'attributes', server_default='{}')
+
+
+def downgrade():
+    """Downgrade: Drop the extras column to the 'db_dbgroup' table"""
+    op.drop_column('db_dbgroup', 'extras')
+
+    op.alter_column('db_dbnode', 'extras', server_default=None)
+    op.alter_column('db_dbnode', 'attributes', server_default=None)
