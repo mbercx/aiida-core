5b2505ae09b90f84d9026a676811e53f039f3c94
Marnik Bercx <mbercx@gmail.com>
2025-04-10 15:25:55 +1000

    🐛 Fix `local_copy_list` behavior for nested target folder
    Co-authored-by: superstar54 <xingwang1991@gmail.com>

--- tests/engine/daemon/test_execmanager.py
+++ tests/engine/daemon/test_execmanager.py
@@ -145,6 +145,7 @@ def test_retrieve_files_from_list(
         (['sub', None], {'b': 'file_b'}),
         ([None, 'target'], {'target': {'sub': {'b': 'file_b'}, 'a': 'file_a'}}),
         (['sub', 'target'], {'target': {'b': 'file_b'}}),
+        (['sub', 'target/another-sub'], {'target': {'another-sub': {'b': 'file_b'}}}),
     ),
 )
 def test_upload_local_copy_list(
@@ -312,6 +313,14 @@ def test_upload_file_copy_operation_order(node_and_calc_info, tmp_path, order, e
 @pytest.mark.parametrize(
     'sandbox_hierarchy, local_copy_list, remote_copy_list, expected_hierarchy, expected_exception',
     [
+        # HP test case
+        (
+            {},
+            ((FolderData, {'out': {'HP': {'file': 'content'}}}, 'out/HP', 'out/HP'),),
+            (),
+            {'out': {'HP': {'file': 'content'}}},
+            None,
+        ),
         ## Single `FileCopyOperation`
         # Only Sandbox
         ({'pseudo': {'Ba.upf': 'Ba pseudo'}}, (), (), {'pseudo': {'Ba.upf': 'Ba pseudo'}}, None),
